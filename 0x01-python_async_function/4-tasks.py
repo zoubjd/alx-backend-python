d923ff2c1e96158a9e993c81cde7e320ebd3f915
zoubjd <zouhairbajdouri19@gmail.com>
2024-10-15 19:31:49 +0100

    finally done

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,11 +2,12 @@
 """async"""
 import random
 import asyncio
+import typing
 
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
+async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
     """asyncs maxdelay n times"""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
